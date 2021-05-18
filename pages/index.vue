3ef9886e9b5e6a9f72e9e74da9b1aadcb1b5f3b7
Diriraro <94skyoun@naver.com>
2021-05-18 17:37:28 +0900

    plz

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <!-- <div class="container">
     <header>
       <h1 class="title">{{title}}</h1>
       <img src="https://miro.medium.com/max/750/1*NTGKGfvCt9bX_S8qqCBgBQ.png" alt="" />
@@ -16,20 +16,57 @@
     <nuxt-link to="/three" target="_blank" class="button--green">
       documentation
     </nuxt-link>
-  </div>
+  </div> -->
+  <section class="container">
+    <ul>
+      <li v-for="item in todos" :key="item.id">
+        <input type="checkbox" :checked="item.done" @change="toggle(item)" />
+        <span :class="{done: item.done}"> {{item.title}} </span>
+        <button @click="remove(item)">지우기</button>
+      </li>
+    </ul>
+    <p> 
+      <input type="text" placeholder="할 일을 적으셈" v-model="todoTitle" @keyup.enter="addTodo" /> 
+      <button @click="addTodo">입력</button> 
+    </p>
+  </section>
 </template>
 
 <script>
+import {mapMutations} from 'vuex';
 export default {
-  data(){
+  // data(){
+  //   return{
+  //     title : 'Nuxt.js를 활용한 서버사이드렌더링이래'
+  //   }
+  // }
+  // ---------------------------------------------------------------------------------------
+  data: function(){
     return{
-      title : 'Nuxt.js를 활용한 서버사이드렌더링이래'
+      todoTitle: '',
+    };
+  },
+  computed: {
+    todos (){
+      return this.$store.state.todo.list;
     }
+  },
+  methods :{
+    addTodo (){
+      this.$store.commit('todo/add', this.todoTitle);
+      this.todoTitle = '';
+    },
+    ...mapMutations({
+      toggle: 'todo/toggle',
+      remove: 'todo/remove'
+    })
   }
+
 }
 </script>
 
 <style>
+.done { text-decoration: line-through; }
 .container {
   margin: 0 auto;
   min-height: 100vh;
